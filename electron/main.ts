06a9090868a7b56e47d562fbf55f15bd2191a527
Gilyoung Kim <xpdlf1004@gmail.com>
2019-09-08 19:05:03 +0900

    Fix the electron.

--- electron/main.ts
+++ electron/main.ts
@@ -7,7 +7,7 @@ if (!app.isPackaged) {
     console.log("Run in development");
     loadURL = serve({ directory: "../dist/" });
 } else {
-    console.log("Run int package");
+    console.log("Run in package");
     loadURL = serve({ directory: "./dist/" });
 }
 
@@ -15,7 +15,11 @@ function createWindow() {
     // Create the browser window.
     const win = new BrowserWindow({ width: 1280, height: 800 });
 
-    loadURL(win);
+    setTimeout(() => {
+        // Wait until electron-serve register the "app://" scheme.
+        // See https://github.com/sindresorhus/electron-serve/issues/18
+        loadURL(win);
+    }, 1000);
 }
 
 app.on("window-all-closed", () => {
